55076f3494d8de879fb225b003ccbb82f2a91e70
Elis Pethke <elispethke@gmail.com>
2023-05-17 18:13:58 +0200

    Initial Commit

---
+++ FP-Viagem/FP_ViagemApp.swift
@@ -0,0 +1,17 @@
+//
+//  FP_ViagemApp.swift
+//  FP-Viagem
+//
+//  Created by Elisangela Pethke on 17.05.23.
+//
+
+import SwiftUI
+
+@main
+struct FP_ViagemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
